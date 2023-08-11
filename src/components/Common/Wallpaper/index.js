51ff3f8fc738968ea7c21c84f89b6a0155f143cf
inggu96 <dldlsrnr123@naver.com>
2023-08-11 15:00:31 +0900

    fix. 이미지경로수정2

--- src/components/Common/Wallpaper/index.js
+++ src/components/Common/Wallpaper/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './wallpaper.module.scss';
+import WallpaperImg from '../../../asset/Images/Wallpaper.jpg';
 
 const Wallpaper = () => {
   return (
@@ -9,7 +10,7 @@ const Wallpaper = () => {
         <p className={styles.text}>
           유튜브에서 듣고싶은 노래를 <br /> 위플리에서 감상하세요.
         </p>
-        <img src="images/Wallpaper.jpg" />
+        <img src={WallpaperImg} />
       </div>
     </article>
   );
